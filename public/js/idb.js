49c48f4d716208bd5245a403dbc41fc9d8737233
Joseph Prospero <jprospero413@gmail.com>
2021-04-24 19:57:03 -0700

    added indexdb offline capability

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,15 +4,16 @@ const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore('budget_tracker', { autoIncrement: true });
+    db.createObjectStore('new_expense', { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
     db = event.target.result;
 
     if (navigator.onLine) {
+        // uploadExpense();
     }
-    alert('Transaction successfully added!')
+    // alert('Transaction successfully added!')
 };
 
 request.onerror = function (event) {
@@ -25,5 +26,44 @@ function saveRecord(record) {
 
     const expenseObjectStore = transaction.objectStore('new_expense');
 
-    expenseObjectStore.add(record); a
+    expenseObjectStore.add(record);
 }
+
+function uploadExpense() {
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    const getAll = expenseObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+                    const expenseObjectStore = transaction.objectStore('new_expense');
+
+                    expenseObjectStore.clear();
+
+                    alert('Transaction successfully added!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+window.addEventListener('online', uploadExpense);
